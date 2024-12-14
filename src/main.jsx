148a476284f5a2a79cab70c79afccd8fcee3cc15
Somnath Kundu <kundusomnath610@gmail.com>
2024-12-15 02:55:11 +0530

    intial commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route>
+        <Route path= "/ " 
+          element={<App />}
+        />
+      </Route>
+
+      <Route>
+        <Route path= "/chat" 
+          element={<h1> This is good page </h1>}
+        />
+      </Route>
+
+
+    </BrowserRouter>
+   
   </StrictMode>,
 )
